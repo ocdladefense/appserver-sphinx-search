45e3764c0a0889be9c907d09ce87e1acce8484fd
Jose Bernal <jbernal.web.dev@gmail.com>
2022-02-16 11:49:37 -0800

    Update core definition files.

--- module.php
+++ module.php
@@ -1,9 +1,25 @@
 <?php
 
-class ModuleTemplateModule extends Module {
+class SphinxModule extends Module {
 
     public function __construct() {
 
         parent::__construct();
+    }
+
+
+
+    /**
+     * @method exampleSearch
+     * 
+     * Example method to connect to a remote SphinxSearch server,
+     * and return search results from a MATCH query.
+     */
+    public function exampleSearch() {
+
+
+
+
+        return "It works!";
     }
 }
