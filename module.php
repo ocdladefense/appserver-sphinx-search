60f4133c54c6fd94e1e8a584ec0be5ce54c1c3aa
Jose Bernal <jbernal.web.dev@gmail.com>
2022-02-16 13:23:13 -0800

    Prepare alternative connection method for Sphinx Search engine.

--- module.php
+++ module.php
@@ -8,14 +8,23 @@ class SphinxModule extends Module {
     }
 
 
+    public function exampleSearch() {
 
+
+
+        $cl = new SphinxClient(); // Will it work?
+
+        
+        return "Hello World!";
+
+    }
     /**
      * @method exampleSearch
      * 
      * Example method to connect to a remote SphinxSearch server,
      * and return search results from a MATCH query.
      */
-    public function exampleSearch() {
+    public function exampleSearchUsingSphinxQL() {
 
         $wgSphinxSearch_host = '172.31.47.173';
         $wgSphinxSearch_port = 9312; // 9306 is our SphinxQL port - let's us use SQL syntax to query the search engine.
