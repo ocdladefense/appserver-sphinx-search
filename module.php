e2fa59a92807c0db6591b59e20c3eaf2ec194802
Jose Bernal <jbernal.web.dev@gmail.com>
2022-02-16 13:45:28 -0800

    function for testing remote database connection.

--- module.php
+++ module.php
@@ -24,9 +24,41 @@ class SphinxModule extends Module {
      * Example method to connect to a remote SphinxSearch server,
      * and return search results from a MATCH query.
      */
-    public function exampleSearchUsingSphinxQL() {
+    public function exampleSearchUsingSphinxQLLocal() {
 
         $wgSphinxSearch_host = '172.31.47.173';
+        $wgSphinxSearch_port = 9312; // 9306 is our SphinxQL port - let's us use SQL syntax to query the search engine.
+
+        $mysqli = new mysqli($wgSphinxSearch_host, "user", "password", "database", 9306);
+
+
+        if ($mysqli->connect_error) die("Connection failed: " . $mysqli->connect_error);
+        // Perform an initial query to the Sphinx search engine.
+
+        // Configure our default database connection.
+        // Database::setDefault();
+
+        // Use this connection to perform a query.
+
+
+        // Iterate through the query results.
+
+
+
+        return "It works!";
+    }
+
+
+    /**
+     * @method exampleSearchUsingSphinxQLRemote
+     * 
+     * Example method to connect to a remote SphinxSearch server,
+     * and return search results from a MATCH query.
+     */
+    public function exampleSearchUsingSphinxQLRemote() {
+
+        $wgSphinxSearch_host = '172.31.47.173'; // Jose will update this IP with the public IP of OCDLA's database / search server.
+
         $wgSphinxSearch_port = 9312; // 9306 is our SphinxQL port - let's us use SQL syntax to query the search engine.
 
         $mysqli = new mysqli($wgSphinxSearch_host, "user", "password", "database", 9306);
