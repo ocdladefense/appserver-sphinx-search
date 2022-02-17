e4969c14e29ffd5607c92fd5d0d7272b72d08dd0
Jose Bernal <jbernal.web.dev@gmail.com>
2022-02-16 16:32:21 -0800

    Test callback to remote OCDLA search instance.

--- module.php
+++ module.php
@@ -57,16 +57,35 @@ class SphinxModule extends Module {
      */
     public function exampleSearchUsingSphinxQLRemote() {
 
-        $wgSphinxSearch_host = '172.31.47.173'; // Jose will update this IP with the public IP of OCDLA's database / search server.
+        $wgSphinxSearch_host = "35.162.222.119"; // Jose will update this IP with the public IP of OCDLA's database / search server.
 
         $wgSphinxSearch_port = 9312; // 9306 is our SphinxQL port - let's us use SQL syntax to query the search engine.
+        // mysql -P9306 -h52.42.123.92 -protocol=tcp --prompt='sphinxQL> '"
+        // $mysqli = new Mysqli("52.42.123.92:9306","","","");
 
-        $mysqli = new mysqli($wgSphinxSearch_host, "user", "password", "database", 9306);
 
+        /*
+        // PDO OPTION 
+        $dsn = 'mysql:host=35.162.222.119;port=9306';
 
-        if ($mysqli->connect_error) die("Connection failed: " . $mysqli->connect_error);
-        // Perform an initial query to the Sphinx search engine.
+        try {
+            $pdo = new PDO($dsn);
+            $this->isConnected = true;
+        } catch (PDOException $e) {
+            throw new Exception($e->getMessage());
+        }
 
+        */
+
+        $link = mysqli_connect("35.162.222.119", "", "", "", 9306);
+        if (!$link) {
+            echo "Error: Unable to connect to MySQL." . "<p>";
+            echo "Debugging errno: " . mysqli_connect_errno() . "<p>";
+            echo "Debugging error: " . mysqli_connect_error() . "<p>";
+            exit;
+        }
+        
+  
         // Configure our default database connection.
         // Database::setDefault();
 
