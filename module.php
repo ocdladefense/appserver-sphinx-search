cab8b428853cfc6bbb0909d08cb89bfaf18206d1
Jose Bernal <jbernal.web.dev@gmail.com>
2022-02-17 09:17:43 -0800

    Method to parse and format a SOQL query IN clause.

--- module.php
+++ module.php
@@ -1,5 +1,8 @@
 <?php
 
+
+
+
 class SphinxModule extends Module {
 
     public function __construct() {
@@ -38,15 +41,23 @@ class SphinxModule extends Module {
      */
     public function exampleSearchUsingSphinxQL() {
 
+        // If we're experimenting then let's not bother returning the theme.
+        $debug = true;
+
         // Jose will update this IP with the public IP of OCDLA's database / search server.
         // This is the Elastic IP of the Database Server.
         $sphinxHost = "35.162.222.119"; 
 
-        // This is our SphinxQL port - let's us use SQL syntax to query the search engine.
+        // This is our SphinxQL port - let's use SQL syntax to query the search engine.
         $sphinxQLPort = 9306; 
 
         // Alternate port when using the included SphinxApi.php library.
         $sphinxApiPort = 9312; 
+
+        // We'll perform a secondary $api query for the actual Salesforce 
+        // products using these IDs.
+        $productIds = array();
+
 
         // Example CLI commands for testing availability of searchd.
         // mysql -P9306 -h35.162.222.119 -protocol=tcp --prompt='sphinxQL> '"
@@ -65,24 +76,178 @@ class SphinxModule extends Module {
         }
         */
 
-        $link = mysqli_connect($sphinxHost, "", "", "", $sphinxQLPort);
-        if (!$link) {
+        $conn = mysqli_connect($sphinxHost, "", "", "", $sphinxQLPort);
+        if (!$conn) {
             echo "Error: Unable to connect to MySQL." . "<p>";
             echo "Debugging errno: " . mysqli_connect_errno() . "<p>";
             echo "Debugging error: " . mysqli_connect_error() . "<p>";
             exit;
         }
         
-  
+        $terms = "ocdla";
+        $ql = "SELECT * FROM ocdla_products WHERE MATCH('%s')";
 
+        $query = sprintf($ql,$terms);
 
-        // Use this connection to perform a query.
+        $result = mysqli_query($conn, $query);
 
+        
 
         // Iterate through the query results.
+        while($row = mysqli_fetch_assoc($result)) {
+            $productIds[] = $row["product_id"];
+            // var_dump($row);
+        }
+
+        // Our list of proudct IDs.
+        var_dump($productIds);
+
+
+        
+
+
+        // Per usual.
+        $api = $this->loadForceApi();
+
+
+        
+        $builder = DatabaseUtils::parse("SELECT Id, Name, Description FROM Product2 WHERE Id = '%s'", $productIds);
+
+        var_dump($builder);exit;
+
+
+        // How will we get this to work when needing to pass an array of Product IDs in?
+        $result = $api->query();
+
+
+        var_dump($result->getRecords());
+
+        exit;  
+        // This is where we can print off the description.
+        foreach($results->getRecords() as $product) {
+
+
+        }
+
+
+        // Now we want to get SNIPPETS with the highlighted search term(s).
+
+        
+
+
+        if($debug) {
+            exit;
+        }
+        else return "Using SphinxQL via Mysqli client library!";
+    }
 
 
 
-        return "Using SphinxQL via Mysqli client library!";
+    public function exampleSearchTest() {
+
+        // If we're experimenting then let's not bother returning the theme.
+        $debug = true;
+
+        // Jose will update this IP with the public IP of OCDLA's database / search server.
+        // This is the Elastic IP of the Database Server.
+        $sphinxHost = "35.162.222.119"; 
+
+        // This is our SphinxQL port - let's use SQL syntax to query the search engine.
+        $sphinxQLPort = 9306; 
+
+        // Alternate port when using the included SphinxApi.php library.
+        $sphinxApiPort = 9312; 
+
+        // We'll perform a secondary $api query for the actual Salesforce 
+        // products using these IDs.
+        $productIds = array();
+
+
+        // Example CLI commands for testing availability of searchd.
+        // mysql -P9306 -h35.162.222.119 -protocol=tcp --prompt='sphinxQL> '"
+        // $mysqli = new Mysqli("35.162.222.119:9306","","","");
+
+
+        /*
+        // PDO OPTION - We can use the PDO library when preferred.
+        $dsn = 'mysql:host=35.162.222.119;port=9306';
+
+        try {
+            $pdo = new PDO($dsn);
+            $this->isConnected = true;
+        } catch (PDOException $e) {
+            throw new Exception($e->getMessage());
+        }
+        */
+
+        $conn = mysqli_connect($sphinxHost, "", "", "", $sphinxQLPort);
+        if (!$conn) {
+            echo "Error: Unable to connect to MySQL." . "<p>";
+            echo "Debugging errno: " . mysqli_connect_errno() . "<p>";
+            echo "Debugging error: " . mysqli_connect_error() . "<p>";
+            exit;
+        }
+        
+        $terms = "ocdla";
+        $ql = "SELECT * FROM ocdla_products WHERE MATCH('%s')";
+
+        $query = sprintf($ql,$terms);
+
+        $result = mysqli_query($conn, $query);
+
+        
+
+        // Iterate through the query results.
+        while($row = mysqli_fetch_assoc($result)) {
+            $productIds[] = $row["product_id"];
+            // var_dump($row);
+        }
+
+        // Our list of proudct IDs.
+        var_dump($productIds);
+
+
+        
+
+
+        // Per usual.
+        $api = $this->loadForceApi();
+
+
+        
+        $builder = DatabaseUtils::parse("SELECT Id, Name, Description FROM Product2 WHERE Id = '%s'", $productIds);
+
+        var_dump($builder);exit;
+
+
+        // How will we get this to work when needing to pass an array of Product IDs in?
+        $result = $api->query();
+
+
+        var_dump($result->getRecords());
+
+        exit;  
+        // This is where we can print off the description.
+        foreach($results->getRecords() as $product) {
+
+
+        }
+
+
+        // Now we want to get SNIPPETS with the highlighted search term(s).
+
+        
+
+
+        if($debug) {
+            exit;
+        }
+        else return "Using SphinxQL via Mysqli client library!";
     }
 }
+
+
+
+
+
+
